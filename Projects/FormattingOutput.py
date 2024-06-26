8d59b9d379447982a9a61e13c177372e9def9640
nithuviswan <nithuviswan@gmail.com>
2024-06-27 00:25:17 +0530

    27June

---
+++ Projects/FormattingOutput.py
@@ -0,0 +1,15 @@
+name = "John"
+age = 25
+salary = 5200000.2
+
+# print(name,age,salary)
+#
+# print("Name is :", name)
+# print("Age is :", age)
+# print("Salary is :", salary)
+
+
+# print ( "Name : %s  Age = %d  Salary = %f" %(name,age,salary))
+
+print("Name : {1} Age : {0} Salary: {2} ".format(age,name,salary))
+
---
+++ Projects/Variables.py
@@ -0,0 +1,33 @@
+a =10
+b= 10.5
+name = "Kumar"
+name1 = 'Pavan'
+
+x =True
+
+# print(a)
+
+#
+# a,b,name = 10,10.5,"pavan"
+#
+# print(a,b,name)
+
+a=b=c=20
+print(a,b,c)
+
+x=10
+y=20
+
+y,x=x,y
+print(x,y)
+
+# import keyword
+# print(keyword.kwlist)
+
+# print("a",'n',"c")
+
+
+num = input("enter the first number : ")
+num1 = input("Enter the second number : ")
+
+print(int(num)+int(num1))
